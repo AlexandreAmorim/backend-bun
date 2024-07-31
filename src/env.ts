ff57cf5f95e751952d8309656c33805ae0cb985e
alexandre amorim <milton.rj30@gmail.com>
2024-07-31 13:16:57 -0300

    env.ts

--- src/env.ts
+++ src/env.ts
@@ -1,12 +1,12 @@
 import { z } from 'zod'
 
 const envSchema = z.object({
-	DATABASE_URL: z.string().url().min(1),
-	JWT_SECRET_KEY: z.string().min(1),
-	API_BASE_URL: z.string().url().min(1),
+	DATABASE_URL: z.string(),
+	JWT_SECRET_KEY: z.string(),
+	API_BASE_URL: z.string().url(),
 	AUTH_REDIRECT_URL: z.string().url(),
-	SES_ACCESS_KEY_ID: z.string().url().min(1),
-	SES_SECRET_ACCESS_KEY: z.string().url().min(1),
+	SES_ACCESS_KEY_ID: z.string(),
+	SES_SECRET_ACCESS_KEY: z.string(),
 })
 
 export const env = envSchema.parse(process.env)
